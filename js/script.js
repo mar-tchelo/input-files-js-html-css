2b99c0a429041f45e2ca9c1e4f0377fdeedf5727
mar-tchelo <marcelo.js10@gmail.com>
2024-05-24 17:19:49 -0300

    Commit in functionalities  javaScrip

---
+++ js/script.js
@@ -0,0 +1,39 @@
+const label = document.querySelector("label");
+
+function onEnter() {
+  label.classList.add("active");
+}
+function onLeave() {
+  label.classList.remove("active");
+}
+
+label.addEventListener("dragenter", onEnter);
+label.addEventListener("drop", onLeave);
+label.addEventListener("dragend", onLeave);
+label.addEventListener("dragleave", onLeave);
+
+const input = document.querySelector("input");
+const dropzone = document.querySelector("#drop-zone");
+
+input.addEventListener("change", (event) => {
+  if (input.files.length > 0) {
+    // console.log(input.files[0])
+    const type = input.files[0].type;
+    const formats = ["image/jpeg", "image/png", "image/jpg"];
+
+    if (!formats.includes(type)) {
+      alert("formato não permitido");
+      return;
+    }
+
+    if (document.querySelector('#cover')) {
+        dropzone.removeChild(document.querySelector('#cover'))
+    }
+
+    const img = document.createElement("img");
+    img.id = "cover";
+    img.src = URL.createObjectURL(input.files[0]);
+
+    dropzone.appendChild(img);
+  }
+});
